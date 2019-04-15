f49d58412f879449d78a5837afbdf16c64c0553d
Radovan <Radovan1993@live.com>
2019-04-15 19:30:14 +0200

    first commit

---
+++ src/radovan/Main.java
@@ -0,0 +1,14 @@
+package radovan;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("June was here");
+		JOptionPane.showMessageDialog(null, "June was here");
+		
+	}
+
+}
